911c54986dfb7ce8b252b0d7bd03032de74d068b
Sasha Klinkemallie <76062492+Gray-SS@users.noreply.github.com>
2023-11-24 21:58:36 +0100

    Added backed

---
+++ Backend/Backend/Models/Party.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class Party
+    {
+        public int PartyId { get; set; }
+
+        public string PartyName { get; set; }
+
+        public Guid PartyAdminId { get; set; }
+    }
+}
