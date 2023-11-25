ef2459f5d38a138bab96b4cc5675b557d3f63160
Sasha Klinkemallie <76062492+Gray-SS@users.noreply.github.com>
2023-11-25 08:58:38 +0100

    Fixed all the problems

--- OG/src/App.jsx
+++ OG/src/App.jsx
@@ -6,6 +6,7 @@ import config from './config.js';
 import * as signalR from '@microsoft/signalr';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { SignalRProvider } from './SignalRContext';
+import { MainPage } from './component/MainPage.jsx';
 
 function App() {
 
@@ -20,7 +21,7 @@ function App() {
 
     newConnection.start()
       .then(() => {
-        console.log('Connexion SignalR r�ussie');
+        console.log('Connexion SignalR réussie');
       })
       .catch((error) => {
         console.error('Erreur: ', error);
@@ -29,7 +30,7 @@ function App() {
     return () => {
       newConnection.stop()
         .then(() => {
-          console.log("Connexion signalR arr�t�e");
+          console.log("Connexion signalR arrêtée");
         })
         .catch((error) => {
           console.error("Erreur lors de l'arret de SignalR: ", error);
@@ -41,7 +42,7 @@ function App() {
     <SignalRProvider connection={connection}>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />}/>
+          <Route path="/" element={<MainPage/>}/>
         </Routes>
       </Router>
     </SignalRProvider>
--- OG/src/component/MainPage.jsx
+++ OG/src/component/MainPage.jsx
@@ -1,4 +1,4 @@
-function mainPage() {
+export function MainPage() {
     return (
         <>
         
@@ -28,5 +28,3 @@ function mainPage() {
         </>
     );
 }
-
-export default mainPage;
