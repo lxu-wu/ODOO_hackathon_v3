3f840dd62a1e9fecdb78bd762f6ce2e7ead8a69f
fenfyp <yoann.nana@gmail.com>
2023-11-25 07:42:23 +0100

    add_icon

--- OG/src/App.jsx
+++ OG/src/App.jsx
@@ -2,28 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      
-      </div>
-      <div>
+    <>   
         <div>
           <img className='logo' src="../public/olymp-logo.jpg" alt="" />
         </div>
         <div>
+          <img src="../public/user972.png" alt="" className='usericon' />
+      
           <input type="text" placeholder='Pseudo' id='pseudo' />
         </div>
         <div>
           <button id='join'>Rejoindre</button>
 
+          
+        </div>
+        <div>
           <button>Creer un tournoi</button>
         </div>
-      </div>
+     
      
     </>
   )
